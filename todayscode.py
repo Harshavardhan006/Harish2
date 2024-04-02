e0a54e2b1f5e37ed8f9750a3fd7bceefa95be526
Harshavardhan006 <84387431+Harshavardhan006@users.noreply.github.com>
2024-04-02 19:08:15 +0530

    Update todayscode.py

--- todayscode.py
+++ todayscode.py
@@ -175,6 +175,24 @@ def findWords(words):
     return list
 words=["Hello","Alaska","Dad","Peace"]
 print(findWords(words))
+#Reverse vowels of string
+def reverseVowels(s):
+    l = 0
+    r = len(s) - 1
+    v = "aeiouAEIOU"
+    s = list(s)
+    while l < r:
+        if s[l] not in v:
+            l += 1
+        if s[r] not in v:
+            r -= 1
+        if s[l] in v and s[r] in v:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+    return ''.join(s)
+s="hello"
+print(reverseVowels(s)) 
 
 
 
