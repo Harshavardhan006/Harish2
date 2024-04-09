589cc353c44f6739de6f372f8932d132e5d0417a
Harshavardhan006 <harishmahes622002@gmail.com>
2024-04-09 12:14:34 +0530

    updatedfile

--- todayscode.py
+++ todayscode.py
@@ -210,4 +210,16 @@ def addDigits(num):
     else:
         return 1 + ((num - 1) % 9)
 num=38
-print(addDigits(num))
+print(addDigits(num))    
+
+#Contain Duplicates
+def containsDuplicate(nums):
+    nums.sort()
+    n = len(nums)
+    for i in range(1, n):
+        if nums[i] == nums[i - 1]:
+            return True
+    return False
+nums=[1,2,3,1]
+print(containsDuplicate(nums))
+
